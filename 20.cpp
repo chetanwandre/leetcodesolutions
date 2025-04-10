ac686d0facefb90d093ab912416d02e0b29ed277
Chetan Wandre <164233886+chetanwandre@users.noreply.github.com>
2025-04-10 16:12:28 +0530

    Create 20.cpp

---
+++ 20.cpp
@@ -0,0 +1,30 @@
+//valid parenthesis using stack
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto it: s) {
+            if(it == '(' || it == '{' || it == '[') {
+                st.push(it);
+            }
+            else {
+                if(st.size() == 0) {
+                    return false;
+                }
+                char ch = st.top();
+                st.pop();
+                if((ch == '[' && it != ']') || 
+                (ch == '{' && it != '}') || 
+                (ch == '(' && it != ')')) {
+                    return false;
+
+                }
+                
+    
+            }
+        }
+
+        return st.empty();
+    }
+};
